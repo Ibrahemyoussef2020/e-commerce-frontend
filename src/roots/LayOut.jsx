64e652452c2d50d74fdd024d45abfd621d0ddac4
unknown <ibrahem.youssef2020@hotmail.com>
2023-01-18 22:21:51 +0200

    loading

--- src/roots/LayOut.jsx
+++ src/roots/LayOut.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
+import { ThreeDots } from 'react-loader-spinner'
 import { Outlet } from 'react-router-dom'
 import Footer from './footer/Footer'
 import Header from './header/Header'
 import Aside from './navAside'
 
 function LayOut() {
-  return (
-    <>
+  const [loading,setLoading] = useState(true)
+
+  useEffect(()=>{
+    setTimeout(()=>setLoading(false),7000)
+  },[loading])
+
+  const OutPut = loading ? <div className='full-center'>
+  <ThreeDots 
+    height="80" 
+    width="80" 
+    radius="9"
+    color="#4fa94d" 
+    ariaLabel="three-dots-loading"
+    wrapperStyle={{}}
+    wrapperClassName=""
+    visible={true}
+  />
+  </div> : <>
     <Header />
         <Aside/>
         <Outlet />
     <Footer />
     </>
-  )
+  
+  return OutPut
 }
 
 export default LayOut
